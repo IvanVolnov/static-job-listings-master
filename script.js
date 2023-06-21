adc4a462c67d43411fdf62300085eea4ed9ff39a
IvanVolnov <ivanvolnov92@gmail.com>
2023-06-22 00:08:54 +0300

    started javasqript

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+"use stict";
+
+const clearBtn = document.querySelector("#clear-filter");
+const closeTag = document.querySelector(".filter");
+
 console.log("connect!");
+fetch("./data.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => console.log(data));
+
+closeTag.addEventListener("click", function (e) {
+  console.log(e);
+});
