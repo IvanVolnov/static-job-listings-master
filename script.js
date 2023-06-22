43aafcc865561d6f146bda32f2a98ed7a8c8beb6
IvanVolnov <ivanvolnov92@gmail.com>
2023-06-22 20:25:13 +0300

    added event listeners

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 "use stict";
 
 const clearBtn = document.querySelector("#clear-filter");
-const closeTag = document.querySelector(".filter");
+const filterTag = document.querySelector(".filter");
+const filterBlockTags = document.querySelector(".tags-list__filters");
+const jobOfferTags = document.querySelector(".tags-list");
 
 console.log("connect!");
 fetch("./data.json")
@@ -10,6 +12,19 @@ fetch("./data.json")
   })
   .then((data) => console.log(data));
 
-closeTag.addEventListener("click", function (e) {
-  console.log(e);
+const ListenClick = function (base, closest, callback) {
+  base.addEventListener("click", function (e) {
+    const item = e.target.closest(closest);
+    if (item && base.contains(item)) {
+      callback();
+    }
+  });
+};
+
+ListenClick(jobOfferTags, ".tag", function () {
+  console.log("rabotaet");
 });
+
+// jobOfferTags.addEventListener("click", function (e) {
+//   console.log(e);
+// });
