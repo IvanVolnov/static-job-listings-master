1cd2d661b29d7769c444219dea8ac38724931e4c
IvanVolnov <ivanvolnov92@gmail.com>
2023-06-26 23:24:33 +0300

    added tag and filter cklicking and updating ui functionality

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ const jobName = document.querySelectorAll(".job-name");
 const jobsList = document.getElementById("jobs-list");
 
 // Get data from json file
-// let data;
+let data;
 const getData = async function (url) {
   try {
-    const res = await fetch("./data.json");
-    const data = await res.json();
+    const res = await fetch(url);
+    data = await res.json();
     console.log("async finished", data);
     renderJobs(data);
   } catch (err) {
@@ -65,41 +65,76 @@ const generateMarkup = function (el) {
 </article>`;
 };
 
-const renderLang = function (arr) {
-  arr.forEach(function (el) {
-    return `<li class="tag">${el}</li>`;
+const renderJobs = function (data) {
+  jobsList.innerHTML = "";
+  data.forEach((el) => {
+    const prop = [el.role, el.level, ...el.languages, ...el.tools];
+    // TODO: rewrite markup using prep array for more readability
+    if (!filters.size || Array.from(filters).every((e) => prop.includes(e)))
+      jobsList.insertAdjacentHTML("beforeend", generateMarkup(el));
   });
 };
 
-const renderJobs = function (data) {
-  data.forEach((el) =>
-    jobsList.insertAdjacentHTML("beforeend", generateMarkup(el))
-  );
-};
-
+//Clear behaviour of job name link
 jobName.forEach((e) =>
   e.addEventListener("click", function (e) {
     e.preventDefault();
   })
 );
 
+// Utilities and helpers
 const ListenClick = function (base, closest, callback) {
   base.addEventListener("click", function (e) {
     const item = e.target.closest(closest);
     if (item && base.contains(item)) {
       callback(e);
-      console.log(e);
     }
   });
 };
 
-ListenClick(jobOfferTags, ".tag", function (e) {
-  console.log(e);
-});
+const updateUI = function () {
+  renderJobs(data);
+  renderTags(filters);
+};
+
+const resetJobsList = function () {};
+//Filtering
+const filters = new Set();
+
+const renderTags = function (set) {
+  filterBlockTags.innerHTML = "";
+  set.forEach((e) => {
+    filterBlockTags.insertAdjacentHTML(
+      "beforeend",
+      `<li class="tag filter">${e}</li>`
+    );
+  });
+};
 
 // jobOfferTags.addEventListener("click", function (e) {
 //   console.log(e);
 // });
 
+// init
+
 getData("./data.json");
-// el.languages.forEach((e) => console.log(`<li class="tag">${e}</li>`));
+
+// events when tag clicked
+ListenClick(jobsList, ".tag", function (e) {
+  let sizeBefore = filters.size;
+  filters.add(e.target.innerHTML);
+  if (!sizeBefore) {
+  } // TODO: show filter block
+  if (sizeBefore != filters.size) {
+    updateUI();
+  }
+
+  console.log(filters, sizeBefore, filters.size);
+});
+
+//event when cross on filter clicked
+ListenClick(filterBlockTags, ".filter", function (e) {
+  filters.delete(e.target.innerHTML);
+  updateUI();
+  // TODO: hide filter block checker
+});
